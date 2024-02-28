23a13956809950f34fd6be107d58510781ccf117
= <=>
2024-02-28 21:12:58 +0700

    Update MainActivity, LoginActivity: Add User Table Constructor

--- app/src/main/java/fpt/edu/fumic/MainActivity.java
+++ app/src/main/java/fpt/edu/fumic/MainActivity.java
@@ -2,30 +2,30 @@ package fpt.edu.fumic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.List;
-
 import fpt.edu.fumic.dao.CategoryDAO;
+import fpt.edu.fumic.dao.UserDAO;
 import fpt.edu.fumic.database.FumicDB;
-import fpt.edu.fumic.model.Category;
+import fpt.edu.fumic.ui.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private FumicDB mFumicDB;
     private CategoryDAO categoryDAO = new CategoryDAO(this);
-
+    private UserDAO userDAO = new UserDAO(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mFumicDB = new FumicDB(this);
-        categoryDAO.getAll();
+        categoryDAO.getAllCategory();
+        userDAO.getAllUser();
         Log.d("MainActivity", "Database status: " + (mFumicDB.getReadableDatabase() != null ? "Created" : "Not created"));
+        Intent intent_login = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent_login);
     }
-
 }
--- app/src/main/java/fpt/edu/fumic/ui/LoginActivity.java
+++ app/src/main/java/fpt/edu/fumic/ui/LoginActivity.java
@@ -2,6 +2,7 @@ package fpt.edu.fumic.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +43,8 @@ public class LoginActivity extends AppCompatActivity {
         bt_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent_register = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent_register);
                 Toast.makeText(LoginActivity.this, "Register page", Toast.LENGTH_SHORT).show();
             }
         });
