3a994b4dd3bbdb815148f3e24b5fc251f1c714c2
= <=>
2024-03-02 14:09:20 +0700

    Update note: Do not commit local master branch to remote master branch, just pull only and use local master branch to test your own function

--- app/src/main/java/fpt/edu/fumic/MainActivity.java
+++ app/src/main/java/fpt/edu/fumic/MainActivity.java
@@ -22,7 +22,5 @@ public class MainActivity extends AppCompatActivity {
         mFumicDB = new FumicDB(this);
         categoryDAO.getAllCategory();
         Log.d("MainActivity", "Database status: " + (mFumicDB.getReadableDatabase() != null ? "Created" : "Not created"));
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
     }
 }
