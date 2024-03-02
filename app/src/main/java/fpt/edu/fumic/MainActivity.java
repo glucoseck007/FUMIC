e3541cb48db7e3bfe0af2a98973c7693eb0c82cb
= <=>
2024-03-02 18:31:12 +0700

    Update: update room database

--- app/src/main/java/fpt/edu/fumic/MainActivity.java
+++ app/src/main/java/fpt/edu/fumic/MainActivity.java
@@ -2,25 +2,16 @@ package fpt.edu.fumic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import fpt.edu.fumic.dao.CategoryDAO;
-import fpt.edu.fumic.database.FumicDB;
-import fpt.edu.fumic.ui.LoginActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private FumicDB mFumicDB;
-    private final CategoryDAO categoryDAO = new CategoryDAO(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFumicDB = new FumicDB(this);
-        categoryDAO.getAllCategory();
-        Log.d("MainActivity", "Database status: " + (mFumicDB.getReadableDatabase() != null ? "Created" : "Not created"));
     }
 }
