8496b6217ae71ad0f52a83de3af77aa4bdc6a184
= <=>
2024-03-12 15:14:16 +0700

    Update

--- app/src/main/java/fpt/edu/fumic/database/dao/BookDAO.java
+++ app/src/main/java/fpt/edu/fumic/database/dao/BookDAO.java
@@ -3,7 +3,6 @@ package fpt.edu.fumic.database.dao;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -13,47 +12,39 @@ import fpt.edu.fumic.database.entity.BookEntity;
 
 @Dao
 public interface BookDAO {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    List<Long> insertAllBooks(List<BookEntity> bookEntities);
+    @Insert
+    void insertAllBooks(List<BookEntity> bookEntities);
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    long insertBook(BookEntity book);
+    @Insert
+    void insertBook(BookEntity book);
 
     @Query("SELECT * FROM BOOK")
     LiveData<List<BookEntity>> loadAllBooks();
 
     @Query("select * from BOOK where status =:status order by dateUpload asc limit :limit offset :offset")
     List<BookEntity> getBookListAvailable(int status,int limit, int offset);
-
-    @Update
-    int updateBook(BookEntity book);
-
-    @Query("SELECT id FROM CATEGORY WHERE name = :name")
-    int getCategoryId(String name);
-
-    @Query("SELECT MAX(id) FROM BOOK")
-    int getLatestBookId();
-
-    @Query("SELECT id FROM BOOK WHERE title = :title")
-    Integer getExistBook(String title);
-
-    @Query("SELECT image FROM BOOK where id = :id")
-    byte[] getImage(int id);
-
-    @Query("DELETE FROM BOOK where id = :id")
-    void deleteBook(int id);
-
+    /*
+    Date 6/3/2024
+    List book
+     */
     @Query("SELECT * FROM BOOK ORDER BY noOfView DESC")
     LiveData<List<BookEntity>> getBooksSortedByViews();
+    /*
+    Date 6/3/2024
+    List book
+     */
     @Query("SELECT * FROM BOOK ORDER BY id")
     LiveData<List<BookEntity>> loadBooksSortedById();
-
+    /*
+    Date 6/3/2024
+    List book
+     */
     @Query("SELECT * FROM BOOK ORDER BY dateUpload DESC")
     LiveData<List<BookEntity>> loadBooksSortedByDate();
 
-    @Query("SELECT * FROM BOOK WHERE title like :key")
+    @Query("SELECT * FROM BOOK WHERE TITLE LIKE :key")
     List<BookEntity> searchByTitle(String key);
 
-    @Query("SELECT * FROM BOOK WHERE title = :title")
-    BookEntity getBookByTitle(String title);
+    @Update
+    void updateBook(BookEntity book);
 }
--- app/src/main/java/fpt/edu/fumic/repository/BookRepository.java
+++ app/src/main/java/fpt/edu/fumic/repository/BookRepository.java
@@ -1,119 +1,70 @@
 package fpt.edu.fumic.repository;
 
 import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 
 import androidx.lifecycle.LiveData;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import fpt.edu.fumic.database.AppDatabase;
-import fpt.edu.fumic.database.dao.AuthorDAO;
 import fpt.edu.fumic.database.dao.BookDAO;
-import fpt.edu.fumic.database.dao.CategoryDAO;
-import fpt.edu.fumic.database.dao.ChapterDAO;
-import fpt.edu.fumic.database.dao.OwnDAO;
-import fpt.edu.fumic.database.entity.AuthorEntity;
 import fpt.edu.fumic.database.entity.BookEntity;
-import fpt.edu.fumic.database.entity.ChapterEntity;
-import fpt.edu.fumic.database.entity.OwnEntity;
 
 public class BookRepository {
 
     private final BookDAO bookDAO;
-    private final CategoryDAO categoryDAO;
-    private final OwnDAO ownDAO;
-    private final AuthorDAO authorDAO;
-    private final ChapterDAO chapterDAO;
-
 
     public BookRepository(Context context) {
         AppDatabase appDatabase = AppDatabase.getInstance(context);
         bookDAO = appDatabase.bookDAO();
-        ownDAO = appDatabase.ownDAO();
-        authorDAO = appDatabase.authorDAO();
-        categoryDAO = appDatabase.categoryDAO();
-        chapterDAO = appDatabase.chapterDAO();
     }
 
-    public List<Long> createBook(List<BookEntity> bookEntities) {return bookDAO.insertAllBooks(bookEntities); }
 
-    public long createBook(BookEntity book) {
-        return bookDAO.insertBook(book);
+    public void createBook(List<BookEntity> bookEntities) {
+        bookDAO.insertAllBooks(bookEntities);
     }
 
-    public String getCategoryNameById(int id) {return categoryDAO.getCategoryNameById(id); }
+    public void createBook(BookEntity book) {
+        bookDAO.insertBook(book);
+    }
 
     public LiveData<List<BookEntity>> getAllBooks() {
         return bookDAO.loadAllBooks();
     }
 
-    public int updateOwn(OwnEntity own) {return ownDAO.updateOwn(own); }
-
-    public List<BookEntity> getBooks(int status, int limit, int offset) {
-        return bookDAO.getBookListAvailable(status, limit, offset);
-    }
-
-    public int updateBook(BookEntity book) {
-        return bookDAO.updateBook(book);
-    }
-
-    public int getLatestBookId() {return bookDAO.getLatestBookId();}
-
-    public Integer getExistBook(String title) {return bookDAO.getExistBook(title); }
-
-    public void deleteBook(int id) { bookDAO.deleteBook(id); }
-
-    public void deleteAuthor(int id) { authorDAO.deleteAuthor(id); }
-
-    public long insertRelationship(int authorId, int bookId) {
-        OwnEntity own = new OwnEntity(authorId, bookId);
-        return ownDAO.insert(own);
-    }
-
-    public long insertAuthor(int authorId, String name) {
-        AuthorEntity author = new AuthorEntity(authorId, name);
-        return authorDAO.insertAuthor(author);
-    }
-    public Integer getLatestAuthorId() {return authorDAO.getLatestAuthorId(); }
-
-    public int getAuthorIdWhoOwn(int id) {return ownDAO.getAuthorIdWhoOwn(id); }
-
-    public String getAuthorById(int id) {return authorDAO.getAuthorById(id); }
-
-    public Integer getExistAuthor(String name) {return authorDAO.getExistAuthor(name); }
-
-    public int getCategoryId(String name) {return bookDAO.getCategoryId(name); }
-
-    public byte[] getImage(int id) {return bookDAO.getImage(id); }
-
-    public int getRelationship(int bookId, int authorId) {return ownDAO.getRelationship(bookId, authorId);}
-
-    public int insertChapterContent(List<ChapterEntity> chapter) {
-        return chapterDAO.insert(chapter).size();
-    }
-
-    public LiveData<List<BookEntity>> getBooksSortedByViews() {
-        return bookDAO.getBooksSortedByViews();
-    }
-
-    public LiveData<List<BookEntity>> getBooksSortedByDate() {
-        return bookDAO.loadBooksSortedByDate();
-    }
-
     public List<BookEntity> getBookListAvailable(int status, int limit, int offset) {
         return bookDAO.getBookListAvailable(status, limit, offset);
-    }
-    public LiveData<List<BookEntity>> getBooksSortedById() {
-        return bookDAO.loadBooksSortedById();
     }
 
     public List<BookEntity> searchByTitle(String key) {
         return bookDAO.searchByTitle(key);
     }
 
-    public BookEntity getBookByTitle(String title) {return bookDAO.getBookByTitle(title); }
+    public void updateBook(BookEntity book) {
+        bookDAO.updateBook(book);
+    }
+
+    /*
+Date 6/3/2024
+List book
+ */
+    public LiveData<List<BookEntity>> getBooksSortedByViews() {
+        return bookDAO.getBooksSortedByViews();
+    }
+
+    /*
+Date 6/3/2024
+List book
+ */
+    public LiveData<List<BookEntity>> getBooksSortedByDate() {
+        return bookDAO.loadBooksSortedByDate();
+    }
+
+    /*
+    Date 6/3/2024
+    List book
+     */
+    public LiveData<List<BookEntity>> getBooksSortedById() {
+        return bookDAO.loadBooksSortedById();
+    }
 }
