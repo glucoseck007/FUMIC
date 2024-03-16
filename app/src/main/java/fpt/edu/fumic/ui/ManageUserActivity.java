84f3f055e482adecd1f02e5dd952a4c0a299eb3e
= <=>
2024-03-16 16:33:17 +0700

    Big update: "Merge branch Linh into Master"

--- app/src/main/java/fpt/edu/fumic/ui/ManageUserActivity.java
+++ app/src/main/java/fpt/edu/fumic/ui/ManageUserActivity.java
@@ -1,14 +1,5 @@
 package fpt.edu.fumic.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-import fpt.edu.fumic.R;
-import fpt.edu.fumic.adapters.UserAdapter;
-import fpt.edu.fumic.database.entity.UserEntity;
-import fpt.edu.fumic.repository.UserRepository;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,11 +8,16 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import fpt.edu.fumic.R;
+import fpt.edu.fumic.adapters.UserAdapter;
+import fpt.edu.fumic.repository.UserRepository;
+
 //delete user
     /*
     Date : 5/3/2024
     */
-public class ManageUserActivity extends AppCompatActivity implements UserAdapter.OnUserClickListener, View.OnClickListener {
+public class ManageUserActivity extends AppCompatActivity implements  View.OnClickListener {
 //public class ManageUserActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView recyclerViewUsers;
@@ -37,40 +33,41 @@ public class ManageUserActivity extends AppCompatActivity implements UserAdapter
         recyclerViewUsers = findViewById(R.id.recycler_view_users);
         recyclerViewUsers.setLayoutManager(new LinearLayoutManager(this));
 
-        ivBack = findViewById(R.id.ivBack); // Initialize ivBack
-        ivBack.setOnClickListener(this); // Set click listener for ivBack
+        ivBack = findViewById(R.id.ivBack);
+        ivBack.setOnClickListener(this);
 
-        userAdapter = new UserAdapter(this);
+        userAdapter = new UserAdapter();
         recyclerViewUsers.setAdapter(userAdapter);
-
         userRepository = new UserRepository(this);
 
-        // Fetch all users from the database
         userRepository.getAllUsers().observe(this, userEntities -> {
             textNoOfUser= findViewById(R.id.text_no_users);
-            // Update the UI with the new list of users
             if (userEntities.isEmpty()) {
-                // Show the TextView if there are no users
                 recyclerViewUsers.setVisibility(View.GONE);
                 textNoOfUser.setVisibility(View.VISIBLE);
             } else {
-                // Show the RecyclerView if there are users
                 recyclerViewUsers.setVisibility(View.VISIBLE);
                 textNoOfUser.setVisibility(View.GONE);
                 userAdapter.setUserList(userEntities);
             }
-            // Update the UI with the new list of users
             userAdapter.setUserList(userEntities);
         });
     }
 
-
-    // Handle user click event (e.g., delete user)
-    @Override
-    public void onUserClick(UserEntity user) {
-        // Handle user deletion here
-        userRepository.deleteUser(user);
-    }
+//    @Override
+//    public void onUserClick(UserEntity user) {
+//        AlertDialog1.showConfirmationDialog(this, "Confirm Deletion", "Are you sure want to delete that book?",
+//                new AlertDialog1.OnConfirmationListener() {
+//                    @Override
+//                    public void onConfirm() {
+//                        userRepository.deleteUser(user);
+//                    }
+//                    @Override
+//                    public void onCancel() {
+//
+//                    }
+//                });
+//    }
 
     @Override
     public void onClick(View v) {
